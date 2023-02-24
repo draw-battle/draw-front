81a1a53d77af44335824ec3e46b1e97ead4df55c
serzh <serzhan181@outlook.com>
2023-02-24 20:11:47 +0600

    redirect if room doesnt exist

--- src/app/(game)/[id]/page.tsx
+++ src/app/(game)/[id]/page.tsx
@@ -4,20 +4,20 @@ import { Canvas } from "@/game/components/canvas";
 import { Chat } from "@/game/components/chat";
 import { Toast } from "@/shared/ui/toast";
 import { useMessagesStore } from "@/store/messages-state";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { io, Socket } from "socket.io-client";
+import { io } from "socket.io-client";
 
 export default function Game() {
+  // TODO: Find better solution to get id query. (useRouter is throwing error in this version of next.)
   const pathname = usePathname();
+  const id = pathname?.replace("/", "");
+
   const [blocked, setBlocked] = useState(false);
   const [timeLeft, setTimeLeft] = useState<number | null>();
 
-  // TODO: Find better solution to get id query. (useRouter is throwing error in this version of next.)
-  const id = pathname?.replace("/", "");
-
   const addMessage = useMessagesStore((state) => state.addMessage);
-  useConnectToRoom({
+  const { connected } = useConnectToRoom({
     roomId: id as string,
     onUserLeave(username) {
       addMessage({
@@ -50,18 +50,20 @@ export default function Game() {
   });
 
   return (
-    <div className="relative flex w-full h-screen gap-3 py-12 mx-auto">
-      {Number(timeLeft) > 0 && (
-        <Toast status="info" text={`Timer: ${timeLeft}`} />
-      )}
-      <div className="absolute inset-0 flex">
-        <Canvas blocked={blocked} />
-      </div>
+    connected && (
+      <div className="relative flex w-full h-screen gap-3 py-12 mx-auto">
+        {Number(timeLeft) > 0 && (
+          <Toast status="info" text={`Timer: ${timeLeft}`} />
+        )}
+        <div className="absolute inset-0 flex">
+          <Canvas blocked={blocked} />
+        </div>
 
-      <div className="absolute w-96 top-5 right-5 bottom-5">
-        <Chat />
+        <div className="absolute w-96 top-5 right-5 bottom-5">
+          <Chat />
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
@@ -80,38 +82,55 @@ const useConnectToRoom = ({
   onDrawPhaseEnd,
   updateTimer,
 }: IUseConnectToRoom) => {
+  const [connected, setConnected] = useState(false);
+  const router = useRouter();
   useEffect(() => {
-    // connection
-    const socket = io("ws://localhost:5000");
+    async function handleRoom() {
+      // check if room exists
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACK_ENDPOINT}/rooms/${roomId}`
+      );
+      const room = await res.json();
+      if (room.statusText !== "OK") {
+        return router.replace("/");
+      }
+      // connection
+      const socket = io("ws://localhost:5000");
 
-    if (socket) {
-      socket.on("connect", () => {
-        socket.emit("join", roomId, "Jamal" + Math.floor(Math.random() * 99));
+      if (socket) {
+        socket.on("connect", () => {
+          setConnected(true);
+          socket.emit("join", roomId, "Jamal" + Math.floor(Math.random() * 99));
 
-        socket.on(
-          "draw phase",
-          ({ ended, timeLeft }: { ended: boolean; timeLeft?: number }) => {
-            if (ended) {
-              onDrawPhaseEnd();
+          socket.on(
+            "draw phase",
+            ({ ended, timeLeft }: { ended: boolean; timeLeft?: number }) => {
+              if (ended) {
+                onDrawPhaseEnd();
+              }
+
+              if (Number.isInteger(timeLeft)) {
+                updateTimer(timeLeft!);
+              }
             }
+          );
 
-            if (Number.isInteger(timeLeft)) {
-              updateTimer(timeLeft!);
-            }
-          }
-        );
+          socket.on("user new", (username) => {
+            onUserNew(username);
+          });
 
-        socket.on("user new", (username) => {
-          onUserNew(username);
+          socket.on("user left", (username) => {
+            onUserLeave(username);
+          });
         });
-
-        socket.on("user left", (username) => {
-          onUserLeave(username);
-        });
-      });
-      return () => {
-        socket.disconnect();
-      };
+        return () => {
+          socket.disconnect();
+        };
+      }
     }
+
+    handleRoom();
   }, []);
+
+  return { connected };
 };
